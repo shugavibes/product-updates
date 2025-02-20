9459580de3ae4bb5ad9239493c484c291f6d45bf
Shuga <alvarezquirosdm@gmail.com>
2025-02-20 15:40:12 -0300

    new fixes on page

--- src/app/updates/[id]/page.tsx
+++ src/app/updates/[id]/page.tsx
@@ -2,14 +2,11 @@ import { notFound } from 'next/navigation';
 import ProductUpdate from '@/components/product-update';
 import { updatesData } from '@/components/product-updates';
 
-type Props = {
-  params: {
-    id: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default async function UpdatePage({ params }: Props) {
+export default function UpdatePage({
+  params,
+}: {
+  params: { id: string }
+}) {
   const update = updatesData.updates.find(u => u.id === params.id);
   
   if (!update) {
