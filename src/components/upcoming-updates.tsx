39c6e78316c0a0960db4e521412258033fa2dd8c
Shuga <alvarezquirosdm@gmail.com>
2025-02-21 18:32:01 -0300

    fixes

--- src/components/upcoming-updates.tsx
+++ src/components/upcoming-updates.tsx
@@ -3,7 +3,6 @@ import { Badge } from '@/components/ui/badge';
 import Image from 'next/image';
 import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
-import { Update } from './product-updates';
 
 const upcomingData = {
   "updates": [
